8302910ce4f4fc9cb9f233e67f2004a67a939d5a
Ronan-Kelley <ronankelley2@gmail.com>
2023-01-27 15:28:04 -0500

    visual mode H/L binds

--- .config/nvim/lua/rhayu/remap.lua
+++ .config/nvim/lua/rhayu/remap.lua
@@ -5,8 +5,10 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- visual block shifting --
+vim.keymap.set("v", "H", "<gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "L", ">gv")
 
 -- make currently loaded file executable --
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
