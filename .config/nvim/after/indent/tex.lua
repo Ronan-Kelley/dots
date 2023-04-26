569b8fe29128f9e990f0636e9e03b0452128a367
Ronan-Kelley <ronankelley2@gmail.com>
2023-04-25 23:39:39 -0400

    changes from school laptop

--- .config/nvim/after/indent/tex.lua
+++ .config/nvim/after/indent/tex.lua
@@ -2,3 +2,4 @@ vim.opt.cindent = false
 vim.opt.smartindent = false
 vim.opt.indentexpr = ""
 vim.opt.autoindent = true
+vim.opt.wrap = true
--- .config/nvim/lua/rhayu/remap.lua
+++ .config/nvim/lua/rhayu/remap.lua
@@ -16,5 +16,11 @@ vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
+-- terminal pane navigation --
+vim.keymap.set("t", "<C-h>", "<C-\\><C-N><C-h>")
+vim.keymap.set("t", "<C-j>", "<C-\\><C-N><C-j>")
+vim.keymap.set("t", "<C-k>", "<C-\\><C-N><C-k>")
+vim.keymap.set("t", "<C-l>", "<C-\\><C-N><C-l>")
+
 -- make currently loaded file executable --
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
