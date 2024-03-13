9a6557ce726272192a19f99dffc3e1f89b0a494e
Ronan-Kelley <ronankelley2@gmail.com>
2024-03-13 16:26:35 -0400

    fix enter not autocompleting

--- .config/nvim/lua/plugins/IDE.lua
+++ .config/nvim/lua/plugins/IDE.lua
@@ -62,7 +62,7 @@ return {
                     documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
                     ['<C-f>'] = cmp_action.luasnip_jump_forward(),
                     ['<C-b>'] = cmp_action.luasnip_jump_backward(),
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
