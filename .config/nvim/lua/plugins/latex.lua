d7e4d5cd397e6ec5f6b398687b9a5db0b6817fdb
Ronan Kelley <ronankelley2@gmail.com>
2025-01-08 16:32:52 -0500

    add latex snippets

--- .config/nvim/lua/plugins/latex.lua
+++ .config/nvim/lua/plugins/latex.lua
@@ -9,6 +9,19 @@ return {
             vim.g.vimtex_context_pdf_viewer = 'zathura'
             vim.g.vimtex_view_method = 'zathura'
             vim.g.vimtex_view_general_viewer = 'zathura'
+            vim.opt.conceallevel = 1
+            vim.g.tex_conceal = 'abdmg'
+        end
+    },
+    {
+        "iurimateus/luasnip-latex-snippets.nvim",
+        dependencies = {
+            "lervag/vimtex",
+            "L3MON4D3/LuaSnip",
+        },
+        config = function()
+            require('luasnip-latex-snippets').setup( { use_treesitter =  true } )
+            require('luasnip').config.setup { enable_autosnippets = true }
         end
     }
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,7 +8,10 @@ return {
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
         "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
+        {
+            "L3MON4D3/LuaSnip",
+            build = "make install_jsregexp"
+        },
         "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
     },
@@ -29,7 +32,6 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
@@ -74,6 +76,7 @@ return {
             snippet = {
                 expand = function(args)
                     require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    require('luasnip').config.setup { enable_autosnippets = true }
                 end,
             },
             mapping = cmp.mapping.preset.insert({
