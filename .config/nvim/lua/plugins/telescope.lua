45488647e2b047aee18919600b4fcbd019a62cc7
Ronan Kelley <ronankelley2@gmail.com>
2025-04-22 17:48:50 -0400

    add binds for git providers

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,8 +6,11 @@ return {
         keys = {
             { "<leader>tb" },
             { "<leader>tf" },
-            { "<leader>tg" },
-            { "<leader>th" }
+            { "<leader>ts" },
+            { "<leader>th" },
+            { "<leader>tgc" },
+            { "<leader>tgb" },
+            { "<leader>tgs" }
         },
         config = function()
             local telescope = require('telescope')
@@ -17,7 +20,10 @@ return {
 
             vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
             vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>tg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>ts', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>tgc', builtin.git_commits, {})
+            vim.keymap.set('n', '<leader>tgb', builtin.git_branches, {})
+            vim.keymap.set('n', '<leader>tgs', builtin.git_status, {})
             vim.keymap.set('n', '<leader>th', ':Telescope heading<CR>')
         end
     }
