ee3f69ded242c25f2775745d6484b07e8f624d04
Ronan Kelley <ronankelley2@gmail.com>
2025-05-06 15:55:47 -0400

    Move trouble to lsp file

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,113 +1,127 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        {
-            "L3MON4D3/LuaSnip",
-            build = "make install_jsregexp"
-        },
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-    },
-
-    config = function()
-        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float)
-
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/nvim-cmp",
+            {
+                "L3MON4D3/LuaSnip",
+                build = "make install_jsregexp"
             },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
+            "saadparwaiz1/cmp_luasnip",
+            "j-hui/fidget.nvim",
+        },
 
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
+        config = function()
+            vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float)
+
+            local cmp = require('cmp')
+            local cmp_lsp = require("cmp_nvim_lsp")
+            local capabilities = vim.tbl_deep_extend(
+                "force",
+                {},
+                vim.lsp.protocol.make_client_capabilities(),
+                cmp_lsp.default_capabilities())
+
+            require("fidget").setup({})
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                },
+                handlers = {
+                    function(server_name) -- default handler (optional)
+                        require("lspconfig")[server_name].setup {
+                            capabilities = capabilities
+                        }
+                    end,
+
+                    zls = function()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.zls.setup({
+                            root_dir = lspconfig.util.root_pattern(".git"),
+                            settings = {
+                                zls = {
+                                    enable_inlay_hints = true,
+                                    enable_snippets = true,
+                                    warn_style = true,
+                                },
                             },
-                        },
-                    })
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "vim" },
+                        })
+                    end,
+                    ["lua_ls"] = function()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.lua_ls.setup {
+                            capabilities = capabilities,
+                            settings = {
+                                Lua = {
+                                    runtime = { version = "Lua 5.1" },
+                                    diagnostics = {
+                                        globals = { "vim" },
+                                    }
                                 }
                             }
                         }
-                    }
-                end,
-            }
-        })
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                    require('luasnip').config.setup { enable_autosnippets = true }
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>']     = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>']     = cmp.mapping.select_next_item(cmp_select),
-                ['<C-b>']     = cmp.mapping.scroll_docs(-4),
-                ['<C-f>']     = cmp.mapping.scroll_docs(4),
-                ['<CR>']      = cmp.mapping.confirm({ select = false }),
-                ["<C-Space>"] = cmp.mapping.confirm({ select = true }),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
+                    end,
+                }
             })
-        })
 
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-        -- luasnip
-        require('luasnip.loaders.from_lua').load({paths = "./lua/snippets"})
-    end
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                        require('luasnip').config.setup { enable_autosnippets = true }
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-p>']     = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>']     = cmp.mapping.select_next_item(cmp_select),
+                    ['<C-b>']     = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>']     = cmp.mapping.scroll_docs(4),
+                    ['<CR>']      = cmp.mapping.confirm({ select = false }),
+                    ["<C-Space>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' }, -- For luasnip users.
+                }, {
+                    { name = 'buffer' },
+                })
+            })
+
+            vim.diagnostic.config({
+                -- update_in_insert = true,
+                float = {
+                    focusable = false,
+                    style = "minimal",
+                    border = "rounded",
+                    source = "always",
+                    header = "",
+                    prefix = "",
+                },
+            })
+
+            -- luasnip
+            require('luasnip.loaders.from_lua').load({paths = "./lua/snippets"})
+        end
+    },
+    {
+        "folke/trouble.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {},
+        cmd = "Trouble",
+        keys = {
+            { "<leader>cd", "<cmd>Trouble diagnostics toggle<cr>" },
+            { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>" },
+        },
+    },
 }
