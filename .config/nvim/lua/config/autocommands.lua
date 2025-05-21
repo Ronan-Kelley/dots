f095c376eb48b56bef7f14c7571e6a961ee1eb70
Ronan Kelley <ronan.kelley@gentex.com>
2025-05-21 13:15:15 -0400

    bind ctrl+space to swap between source/header in c files

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -7,13 +7,16 @@ vim.api.nvim_create_autocmd({"BufWritePost"}, {
 })
 
 -- automatically disable lsp if there is no compile_commands.json
--- and we're in a c file
+-- and we're in a c file. If there is a compile_commands.json, bind
+-- normal mode CTRL+SPACE to swap header/source
 vim.api.nvim_create_autocmd({"LspAttach"}, {
     pattern = { "*.c", "*.h" },
     callback = function()
         if (vim.fn.filereadable("compile_commands.json") == 0)
         then
             vim.cmd([[LspStop]])
+        else
+            vim.keymap.set("n", "<C-space>", ":LspClangdSwitchSourceHeader<CR>")
         end
     end
 })
