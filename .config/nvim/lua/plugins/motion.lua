bf8f12dc07a94d4917332eb71a7447dee8a51a9a
Ronan Kelley <ronankelley2@gmail.com>
2025-05-25 16:41:17 -0400

    Add motion plugins

---
+++ .config/nvim/lua/plugins/motion.lua
@@ -0,0 +1,21 @@
+return
+{
+    {
+        -- peek lines you're jumping to with :<number>
+        'nacro90/numb.nvim',
+        lazy = false,
+        config = function()
+            require('numb').setup()
+        end,
+    },
+    {
+        -- better movement for w/e/b
+        'chrisgrieser/nvim-spider',
+        lazy = true,
+        keys = {
+            { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+            { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+            { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+        }
+    },
+}
