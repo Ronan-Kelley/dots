71b4c13c7d9f737cca02e0bfd6a62eca10718f8a
Ronan Kelley <ronan.kelley@gentex.com>
2025-06-02 10:31:28 -0400

    Clean up gitsigns config

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,25 +2,17 @@ return {
     {
         "lewis6991/gitsigns.nvim",
         lazy = false,
-        -- no reason to set up associated keys if the plugin isn't lazy loaded
-        -- keys = {
-        --     {"<leader>hn"}, -- hunk next
-        --     {"<leader>hN"}, -- hunk previous
-        --     {"<leader>hs"}, -- hunk stage
-        --     {"<leader>hS"}, -- hunk undo stage
-        --     {"<leader>hp"}, -- hunk preview (inline)
-        --     {"<leader>hP"}, -- hunk preview
-        --     {"<leader>hr"}, -- hunk reset
-        -- },
         config = function()
-            require('gitsigns').setup()
             local gsigns = require('gitsigns')
-            gsigns.setup()
+            gsigns.setup{
+                diff_opts = {
+                    vertical = true -- doesn't seem to work
+                }
+            }
 
             vim.keymap.set("n", "<leader>hn", function() gsigns.nav_hunk('next') end)
             vim.keymap.set("n", "<leader>hN", function() gsigns.nav_hunk('prev') end)
             vim.keymap.set("n", "<leader>hs", gsigns.stage_hunk)
-            vim.keymap.set("n", "<leader>hS", gsigns.undo_stage_hunk)
             vim.keymap.set("n", "<leader>hp", gsigns.preview_hunk_inline)
             vim.keymap.set("n", "<leader>hP", gsigns.preview_hunk)
             vim.keymap.set("n", "<leader>hr", gsigns.reset_hunk)
