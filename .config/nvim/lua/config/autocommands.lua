4ae3dc67c81e6e3c23a875c31cf782bf39224b19
Ronan Kelley <ronan.kelley@gentex.com>
2025-06-11 16:41:16 -0400

    remove header swap bind since it screws with neorg

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -7,16 +7,13 @@ vim.api.nvim_create_autocmd({"BufWritePost"}, {
 })
 
 -- automatically disable lsp if there is no compile_commands.json
--- and we're in a c file. If there is a compile_commands.json, bind
--- normal mode CTRL+SPACE to swap header/source
+-- and we're in a c file.
 vim.api.nvim_create_autocmd({"LspAttach"}, {
     pattern = { "*.c", "*.h" },
     callback = function()
         if (vim.fn.filereadable("compile_commands.json") == 0)
         then
             vim.cmd([[LspStop]])
-        else
-            vim.keymap.set("n", "<C-space>", ":LspClangdSwitchSourceHeader<CR>")
         end
     end
 })
