79aa29ac4bd9953feabf854a8741808699fd3289
Ronan Kelley <ronan.kelley@gentex.com>
2025-06-23 16:10:55 -0400

    Remove nvim-spider

--- .config/nvim/lua/plugins/motion.lua
+++ .config/nvim/lua/plugins/motion.lua
@@ -2,7 +2,4 @@
 local function _1_()
   return require("numb").setup()
 end
-local function _2_()
-  return require("spider").setup({customPatterns = {}, subwordMovement = true, consistentOperatorPending = false, skipInsignificantPunctuation = false})
-end
-return {{"nacro90/numb.nvim", config = _1_, lazy = false}, {"chrisgrieser/nvim-spider", lazy = true, keys = {{"w", "<cmd>lua require('spider').motion('w')<CR>", mode = {"n", "o", "x"}}, {"e", "<cmd>lua require('spider').motion('e')<CR>", mode = {"n", "o", "x"}}, {"b", "<cmd>lua require('spider').motion('b')<CR>", mode = {"n", "o", "x"}}}, config = _2_}}
+return {{"nacro90/numb.nvim", config = _1_, lazy = false}}
