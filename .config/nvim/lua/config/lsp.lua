e1c1fb0b3b1672ea8055633fd9af3cfe616774e3
Ronan Kelley <ronan.kelley@gentex.com>
2026-04-20 10:57:58 -0400

    enable ty and ruff LSP configs

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,4 +1,10 @@
-local lsps = { 'lua_ls', 'clangd', 'rust_analyzer' }
+local lsps = {
+    'lua_ls',
+    'clangd',
+    'rust_analyzer',
+    'ty',
+    'ruff'
+}
 
 for _, name in ipairs(lsps) do
     vim.lsp.enable(name)
